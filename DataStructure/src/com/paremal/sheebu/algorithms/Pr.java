cd1ef5a673481f09fe485f5878f1174d6d4c5076
Sheebu.p <Sheebu.p@ivldsp.com>
2021-01-26 10:06:08 +0530

    max valued hour glass from two dimensional array

---
+++ DataStructure/src/com/paremal/sheebu/algorithms/Pr.java
@@ -0,0 +1,14 @@
+package com.paremal.sheebu.algorithms;
+
+public class Pr {
+	
+	public static void main(String[] args) {
+		int [][] arr= {{1,1,1},{1,1,1},{1,1,1},{1,5,3}};
+		System.out.println(arr.length);
+		System.out.println(arr[2].length);
+		System.out.println(arr[3][2]);
+		
+	}
+	
+
+}
