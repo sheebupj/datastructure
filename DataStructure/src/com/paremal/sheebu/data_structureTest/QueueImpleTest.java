ff5ed4d902d005a1aeaf11f32b18f98ba456c6a5
Sheebu.p <Sheebu.p@ivldsp.com>
2021-01-26 10:21:08 +0530

    commented out test cases

--- DataStructure/src/com/paremal/sheebu/data_structureTest/QueueImpleTest.java
+++ DataStructure/src/com/paremal/sheebu/data_structureTest/QueueImpleTest.java
@@ -1,9 +1,7 @@
 package com.paremal.sheebu.data_structureTest;
 
-import static org.junit.Assert.*;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
 
 import com.paremal.sheebu.data_structure.queue.QueueImpl;
 /**
@@ -13,40 +11,40 @@ import com.paremal.sheebu.data_structure.queue.QueueImpl;
  */
 public class QueueImpleTest {
 	
-	private static QueueImpl queue ;
-	@BeforeClass
-	public static void setBeforClass(){
-		queue= new QueueImpl(10);
-	}
-	
-
-	@Test
-	public void isEmptyTest() {
-		
-		assertEquals(true, queue.isEmpty());
-		queue.enqueue(6);
-		assertFalse(queue.isEmpty());
-	}
-	
-	@Test
-	public void sizeTest() {
-		
-		queue= new QueueImpl(10);
-		assertEquals(null,queue.dqueue());
-		for(int i=0;i<10;i++){
-			queue.enqueue(i);
-		}
-		assertEquals(10, queue.size());
-		assertFalse(queue.enqueue(1));
-		queue.dqueue();
-		queue.enqueue(2);
-		assertEquals(10, queue.size());
-		for(int i=0;i<9;i++){
-			queue.dqueue();
-		}
-		assertEquals(1, queue.size());
-		queue.dqueue();
-		assertEquals(0,queue.size());
-	}
+//	private static QueueImpl queue ;
+//	@BeforeClass
+//	public static void setBeforClass(){
+//		queue= new QueueImpl(10);
+//	}
+//	
+//
+//	@Test
+//	public void isEmptyTest() {
+//		
+//		assertEquals(true, queue.isEmpty());
+//		queue.enqueue(6);
+//		assertFalse(queue.isEmpty());
+//	}
+//	
+//	@Test
+//	public void sizeTest() {
+//		
+//		queue= new QueueImpl(10);
+//		assertEquals(null,queue.dqueue());
+//		for(int i=0;i<10;i++){
+//			queue.enqueue(i);
+//		}
+//		assertEquals(10, queue.size());
+//		assertFalse(queue.enqueue(1));
+//		queue.dqueue();
+//		queue.enqueue(2);
+//		assertEquals(10, queue.size());
+//		for(int i=0;i<9;i++){
+//			queue.dqueue();
+//		}
+//		assertEquals(1, queue.size());
+//		queue.dqueue();
+//		assertEquals(0,queue.size());
+//	}
 
 }
