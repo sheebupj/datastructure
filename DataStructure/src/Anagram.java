9cbede6c1b9d34385892af40a3b19c7be5ec42cc
Sheebu.p <Sheebu.p@ivldsp.com>
2021-02-03 14:44:35 +0530

    comma seprated joining added

--- DataStructure/src/Anagram.java
+++ DataStructure/src/Anagram.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Anagram {
@@ -20,12 +21,19 @@ public class Anagram {
 		
 	}
 	
+	public static void joiningTheListComaSeperated(List<String> wordList) {
+		
+		System.out.println(wordList.stream().collect(Collectors.joining(", ")));
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println(" 'dog' 'dog'  anagram="+checkAnagram("dog", "dog"));
 		System.out.println(" 'god' 'dog' anagram="+checkAnagram("god", "dog"));
 		System.out.println(" 'mary' 'army' anagram="+checkAnagram("mary", "army"));
 		System.out.println(" 'army' 'army' anagram="+checkAnagram("army", "army"));
+		
+		joiningTheListComaSeperated(Arrays.asList("dog","god","army","marry"));
 		
 	}
 	
