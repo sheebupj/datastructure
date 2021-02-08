629cc9609991f87c538684503a44f08def378325
Sheebu.PJ <sheebupj@gmail.com>
2021-02-08 05:51:03 +0530

    comment added for stack implementation

--- DataStructure/src/com/paremal/sheebu/data_structure/stack/ArrayStack.java
+++ DataStructure/src/com/paremal/sheebu/data_structure/stack/ArrayStack.java
@@ -2,30 +2,49 @@ package com.paremal.sheebu.data_structure.stack;
 
 public class ArrayStack<E> implements Stack<E> {
 	
+	/**
+	 * Capacity
+	 */
 	public static final int CAPACITY=1000;
+	/**
+	 * internal storage array for stack
+	 */
 	private E[] data;
+	/**
+	 * position indicator for stack
+	 */
 	private int t=-1;
 	
 	
 
+	/**
+	 * default constructor
+	 */
 	public ArrayStack() {
 	
 		this(CAPACITY);
 	}
 	
-
+	/**
+	 * constructor with capacity argument
+	 * @param capacity
+	 */
 	public ArrayStack(int capacity) {
 		super();
 		data=(E[]) new Object[capacity];
 	}
 
-
+	/**
+	 * returns size of stack
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return t+1;
 	}
-
+	/**
+	 * check whethor stack is empty or not
+	 */
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
@@ -34,6 +53,9 @@ public class ArrayStack<E> implements Stack<E> {
 
 
 
+	/**
+	 * returns top elements in the stack
+	 */
 	@Override
 	public E top() {
 		// TODO Auto-generated method stub
@@ -41,6 +63,10 @@ public class ArrayStack<E> implements Stack<E> {
 		
 		return data[t];
 	}
+	
+	/**
+	 * method for pushing elements in the stack
+	 */
 	@Override
 	public void push(E e)throws IllegalStateException  {
 		// TODO Auto-generated method stub
@@ -51,6 +77,9 @@ public class ArrayStack<E> implements Stack<E> {
 
 	}
 
+	/**
+	 * return element from stack
+	 */
 	@Override
 	public E pop() {
 		// TODO Auto-generated method stub
