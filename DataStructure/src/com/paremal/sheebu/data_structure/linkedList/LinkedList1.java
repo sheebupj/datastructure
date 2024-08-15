fe150f9819e89281e21d28314aa5346a656926df
Sheebu PJ <sheebupj@gmail.com>
2024-08-15 18:34:42 +0530

    insert a Node in linked list

--- DataStructure/src/com/paremal/sheebu/data_structure/linkedList/LinkedList1.java
+++ DataStructure/src/com/paremal/sheebu/data_structure/linkedList/LinkedList1.java
@@ -4,13 +4,7 @@ public class LinkedList1 {
     Node first;
     Node last;
 
-    public static void main(String[] args) {
-        LinkedList1 linkedList1= new LinkedList1();
-        linkedList1.add(4);
-        linkedList1.add(7);
-        linkedList1.add(2);
-        linkedList1.printNodes();
-    }
+
     private static class Node{
         Integer data;
         Node next;
@@ -29,13 +23,47 @@ public class LinkedList1 {
             last=newNode;
         }
         else {
-            Node f = first;
-            while (f.next != null) {
-                f = f.next;
+           Node current=first;
+            while (current.next!=null){
+                current=current.next;
             }
-            f.next = newNode;
-            last = newNode;
+            current.next=newNode;
         }
+    }
+    public static void main(String[] args) {
+        LinkedList1 linkedList1= new LinkedList1();
+        linkedList1.add(1);
+        linkedList1.add(9);
+        linkedList1.add(0);
+        linkedList1.insertAt(2,2);
+        linkedList1.insertAt(3,3);
+        linkedList1.insertAt(6,6);
+        linkedList1.printNodes();
+    }
+    void insertAt(Integer pos,Integer data){
+        Node newNode=new Node(data);
+
+        if(first==null){
+            System.out.println("empty linked list");
+        }
+        int count=2;
+        Node current=first;
+        while(count<pos&&current.next!=null ){
+            current=current.next;
+            count++;
+        }
+        if(current.next!=null) {
+            Node temp = current.next;
+            current.next = newNode;
+            newNode.next = temp;
+        }
+        else{
+            System.out.println("end of the linked list reached , adding  data at end of the list");
+            add(data);
+        }
+
+
+        System.out.println();
     }
     void printNodes(){
         Node node =first;
