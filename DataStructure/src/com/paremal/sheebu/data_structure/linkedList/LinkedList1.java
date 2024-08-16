62b2f708ecbfba6fb4410f4552a0509fd15e022b
Sheebu PJ <sheebupj@gmail.com>
2024-08-16 16:45:29 +0530

    delete methods added

--- DataStructure/src/com/paremal/sheebu/data_structure/linkedList/LinkedList1.java
+++ DataStructure/src/com/paremal/sheebu/data_structure/linkedList/LinkedList1.java
@@ -5,74 +5,119 @@ public class LinkedList1 {
     Node last;
 
 
-    private static class Node{
+    private static class Node {
         Integer data;
         Node next;
 
-        Node(Integer data){
-            this.data=data;
-            this.next=null;
+        Node(Integer data) {
+            this.data = data;
+            this.next = null;
         }
 
     }
-    void add(Integer data){
-        Node newNode=new Node(data);
 
-        if (first==null){
-            first=newNode;
+    void add(Integer data) {
+        Node newNode = new Node(data);
+
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            Node current = first;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
             last=newNode;
         }
-        else {
-           Node current=first;
-            while (current.next!=null){
-                current=current.next;
-            }
-            current.next=newNode;
-        }
     }
-    public static void main(String[] args) {
-        LinkedList1 linkedList1= new LinkedList1();
-        linkedList1.add(1);
-        linkedList1.add(9);
-        linkedList1.add(0);
-        linkedList1.insertAt(2,2);
-        linkedList1.insertAt(3,3);
-        linkedList1.insertAt(6,6);
-        linkedList1.printNodes();
-    }
-    void insertAt(Integer pos,Integer data){
-        Node newNode=new Node(data);
 
-        if(first==null){
-            System.out.println("empty linked list");
+    void insertAtBeginning(Integer data) {
+        Node newNode = new Node(data);
+        newNode.next = first;
+        first = newNode;
+    }
+    void insertAt(Integer pos, Integer data) {
+        Node newNode = new Node(data);
+
+        if (first == null) {
+            System.out.println("Empty linked list...Can not insert, aborting");
+            return;
         }
-        int count=2;
-        Node current=first;
-        while(count<pos&&current.next!=null ){
-            current=current.next;
+        int count = 2;
+        Node current = first;
+        while (count < pos && current.next != null) {
+            current = current.next;
             count++;
         }
-        if(current.next!=null) {
+        if (current.next != null) {
             Node temp = current.next;
             current.next = newNode;
             newNode.next = temp;
-        }
-        else{
-            System.out.println("end of the linked list reached , adding  data at end of the list");
+        } else {
+            System.out.println("Insert position greater than LinkedList length , adding  data:"+data+ " at end of the LinkedList");
             add(data);
         }
 
-
-        System.out.println();
     }
-    void printNodes(){
-        Node node =first;
-        System.out.printf("[ ");
-        while(node!=null){
-            System.out.printf(node.data +" ");
-            node=node.next;
+    void deleteFirstNode(){
+        if(first==null){
+            System.out.println("Empty linkedList Nothing to delete");
+            return;
+        }
+        if(first.next!=null) {
+            first = first.next;
+        }
+        else{
+            first=null;
+            last=null;
+        }
+    }
+    void deleteAtPosition(Integer position){
+        if(position==1){
+            deleteFirstNode();
+            return;
+        }
+        int count=1;
+        Node current=first;
+        while(count<position-1&&current.next!=null){
+            current=current.next;
+            count++;
+        }
+        current.next=current.next.next;
+        if(current.next==null){
+            last=current;
+        }
+    }
+
+
+    void printNodes() {
+        Node node = first;
+        System.out.printf("\n[ ");
+        while (node != null) {
+            System.out.printf(node.data + " ");
+            node = node.next;
         }
         System.out.printf("]");
 
+    }
+    public static void main(String[] args) {
+        LinkedList1 linkedList1 = new LinkedList1();
+        linkedList1.deleteFirstNode();
+        linkedList1.insertAt(1,8);
+        linkedList1.add(1);
+        linkedList1.add(9);
+        linkedList1.add(0);
+        linkedList1.insertAtBeginning(100);
+        linkedList1.insertAt(2, 2);
+        linkedList1.insertAt(3, 3);
+        linkedList1.insertAt(10, 6);
+        linkedList1.printNodes();
+        linkedList1.deleteFirstNode();
+        linkedList1.printNodes();
+        linkedList1.deleteAtPosition(1);
+        linkedList1.printNodes();
+        linkedList1.deleteAtPosition(5);
+        linkedList1.printNodes();
     }
 }
