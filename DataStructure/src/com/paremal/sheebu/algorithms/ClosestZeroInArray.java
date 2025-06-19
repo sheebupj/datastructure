34f6a964b60cd2a92a269e1f92f79b3bc342fe05
Sheebu PJ <sheebupj@gmail.com>
2025-06-19 10:36:55 +0530

    closest zero in an array using stream api

--- DataStructure/src/com/paremal/sheebu/algorithms/ClosestZeroInArray.java
+++ DataStructure/src/com/paremal/sheebu/algorithms/ClosestZeroInArray.java
@@ -1,12 +1,17 @@
 package com.paremal.sheebu.algorithms;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
 import static java.lang.Math.abs;
 
 public class ClosestZeroInArray {
     public static void main(String[] args) {
         int[] data = {2, 3, -2, 4, -5,-1,1};
         int[] result=findClosestToZero(data);
+        int[] result1=findClosestToZeroUsingStreamApi(data);
         System.out.println("Closest number to zero: " +result[0]+ " :" +result[1]);
+        System.out.println("Closest number to zero: " +result1[0]+ " :" +result1[1]);
     }
     /*
     An Array of integers is given, both +ve and -ve.
@@ -18,7 +23,6 @@ public class ClosestZeroInArray {
         cz[0]=abs(numbers[0]);
         cz[1]=abs(numbers[1]);
         int cn=cz[0]+cz[1];
-
         for(int i=0;i<numbers.length;i++){
             for(int j=i+1;j<numbers.length;j++){
                 //math.abs() is used to get closest to 0 since we are checking lowest no  which include negetives
@@ -28,11 +32,27 @@ public class ClosestZeroInArray {
                     cn=abs(cz[0])+abs(cz[1]);
                 }
             }
-
         }
-
-
+       return cz;
+    }
+    public static int[] findClosestToZeroUsingStreamApi(int[] numbers) {
+        int[] cz= new int[2];
+        cz[0]=abs(numbers[0]);
+        cz[1]=abs(numbers[1]);
+        AtomicInteger cn= new AtomicInteger(cz[0] + cz[1]);
+        int size=numbers.length;
+        IntStream.iterate(0,i->i<size,i->i=i+1)
+                .forEach(i->{
+                    IntStream.iterate( i+1,j-> j<size,j-> j=j+1)
+                            .forEach(j-> {
+                                if(cn.get() >abs(numbers[i])+abs(numbers[j])){
+                                    cz[0]=numbers[i];
+                                    cz[1]=numbers[j];
+                                    cn.set(abs(cz[0]) + abs(cz[1]));
+                                }
+                            });
+                });
         return cz;
     }
-
+    
 }
