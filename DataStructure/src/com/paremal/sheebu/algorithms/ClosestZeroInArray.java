9d007f5f3761d564aac30a858dd228f40de5cbf1
Sheebu PJ <sheebupj@gmail.com>
2025-06-19 17:09:39 +0530

    closest zero pair from array of integers using stream api

--- DataStructure/src/com/paremal/sheebu/algorithms/ClosestZeroInArray.java
+++ DataStructure/src/com/paremal/sheebu/algorithms/ClosestZeroInArray.java
@@ -1,5 +1,8 @@
 package com.paremal.sheebu.algorithms;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
 import static java.lang.Math.abs;
 
 public class ClosestZeroInArray {
@@ -7,6 +10,8 @@ public class ClosestZeroInArray {
         int[] data = {2, 3, -2, 4, -5,-1,1};
         int[] result=findClosestToZero(data);
         System.out.println("Closest number to zero: " +result[0]+ " :" +result[1]);
+        int[] result1=findClosestToZeroUsingStreamApi(data);
+        System.out.println("Closest number to zero: " +result1[0]+ " :" +result1[1]);
     }
     /*
     An Array of integers is given, both +ve and -ve.
@@ -30,8 +35,31 @@ public class ClosestZeroInArray {
             }
 
         }
+        return cz;
+    }
 
-
+    /*
+   An Array of integers is given, both +ve and -ve.
+   To find the two elements such that their
+    sum is closest to zero Using Stream api.
+    */
+    public static int[] findClosestToZeroUsingStreamApi(int[] numbers) {
+        int[] cz= new int[2];
+        cz[0]=abs(numbers[0]);
+        cz[1]=abs(numbers[1]);
+        AtomicInteger cn= new AtomicInteger(cz[0] + cz[1]);
+        int size=numbers.length;
+        IntStream.iterate(0, i->i<size, i->i=i+1)
+                .forEach(i->{
+                    IntStream.iterate( i+1,j-> j<size,j-> j=j+1)
+                            .forEach(j-> {
+                                if(cn.get() >abs(numbers[i])+abs(numbers[j])){
+                                    cz[0]=numbers[i];
+                                    cz[1]=numbers[j];
+                                    cn.set(abs(cz[0]) + abs(cz[1]));
+                                }
+                            });
+                });
         return cz;
     }
 
