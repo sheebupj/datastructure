0e4879c3f216f7034540b3d09366491b6f1ea483
Sheebu PJ <sheebupj@gmail.com>
2025-06-23 15:02:49 +0530

    Reverse String in each word of a sentence added

--- DataStructure/src/com/paremal/sheebu/algorithms/ReverseString.java
+++ DataStructure/src/com/paremal/sheebu/algorithms/ReverseString.java
@@ -2,17 +2,21 @@ package com.paremal.sheebu.algorithms;
 
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ReverseString {
     public static void main(String[] args) {
         ReverseString reverseString=new ReverseString();
         String numberString= "0123456789";
+        String numberString1= "012 345 6789";
         String reversed1= reverseString.reverseWithoutInbuiltIn(numberString);
         String reversed2= reverseString.reverseWithoutInbuiltInUsingStreamApi(numberString);
-        System.out.println(numberString+":"+reversed1);
-        System.out.println(numberString+":"+reversed2);
+        String reversed3= reverseString.reverseEachWords(numberString1);
+        System.out.println(numberString+" : "+reversed1);
+        System.out.println(numberString+" : "+reversed2);
+        System.out.println(numberString1+" : "+reversed3);
     }
-
+   
     public String reverseWithoutInbuiltIn(String str){
         String resversed="";
 
@@ -29,5 +33,13 @@ public class ReverseString {
                .map(str::charAt)
                .map(String::valueOf)
                .collect(Collectors.joining());
+    }
+    public  String reverseEachWords(String strLine){
+        String[] strArray=strLine.split(" ");
+        ReverseString reverseString= new ReverseString();
+        return Stream.of(strArray)
+                .map(reverseString::reverseWithoutInbuiltInUsingStreamApi)
+                .map(s->s.concat(" "))
+                .collect(Collectors.joining());
     }
 }
