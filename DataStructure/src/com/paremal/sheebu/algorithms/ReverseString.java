10f3ef7701e3fd4c78ae2044292047640be92c9a
Sheebu PJ <sheebupj@gmail.com>
2025-06-26 13:51:34 +0530

    Reverse String in each word of a sentence added

--- DataStructure/src/com/paremal/sheebu/algorithms/ReverseString.java
+++ DataStructure/src/com/paremal/sheebu/algorithms/ReverseString.java
@@ -6,40 +6,78 @@ import java.util.stream.Stream;
 
 public class ReverseString {
     public static void main(String[] args) {
-        ReverseString reverseString=new ReverseString();
-        String numberString= "0123456789";
-        String numberString1= "012 345 6789";
-        String reversed1= reverseString.reverseWithoutInbuiltIn(numberString);
-        String reversed2= reverseString.reverseWithoutInbuiltInUsingStreamApi(numberString);
-        String reversed3= reverseString.reverseEachWords(numberString1);
-        System.out.println(numberString+" : "+reversed1);
-        System.out.println(numberString+" : "+reversed2);
-        System.out.println(numberString1+" : "+reversed3);
-    }
-   
-    public String reverseWithoutInbuiltIn(String str){
-        String resversed="";
+        ReverseString reverseString = new ReverseString();
+        String numberString = "0123456789";
+        String numberString1 = "012 345 6789";
+        String reversed1 = reverseString.reverseWithoutInbuiltIn(numberString);
+        String reversed2 = reverseString.reverseWithoutInbuiltInUsingStreamApi(numberString);
+        String reversed3 = reverseString.reverseEachWords(numberString1);
+        String reversed4 = reverseString.reverseEachWordsInALine(numberString1);
+        System.out.println(numberString + " : " + reversed1);
+        System.out.println(numberString + " : " + reversed2);
+        System.out.println(numberString1 + " : " + reversed3);
+        System.out.println(numberString1 + " : " + reversed4);
 
-        for(int i=str.length()-1;i>=0;i--){
-            resversed=resversed+str.charAt(i);
+    }
+
+
+    /*
+    Reverse a String without using java built in method reverse(in StringBuilder)
+    abcd become dcba
+     */
+    public String reverseWithoutInbuiltIn(String str) {
+        String resversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            resversed = resversed + str.charAt(i);
 
         }
         return resversed;
     }
-    public String reverseWithoutInbuiltInUsingStreamApi(String str){
 
-       return IntStream.iterate(str.length()-1,i-> i>=0,i->i=i-1)
-               .boxed()
-               .map(str::charAt)
-               .map(String::valueOf)
-               .collect(Collectors.joining());
+    /*
+     Reverse a String without using java built in method reverse(in StringBuilder)
+     using stream api
+     abcd become dcba
+      */
+    public String reverseWithoutInbuiltInUsingStreamApi(String str) {
+
+        return IntStream.iterate(str.length() - 1, i -> i >= 0, i -> i = i - 1)
+                .boxed()
+                .map(str::charAt)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
-    public  String reverseEachWords(String strLine){
-        String[] strArray=strLine.split(" ");
-        ReverseString reverseString= new ReverseString();
+
+    /*
+     Reverse each word in a line  without using java built in method reverse(in StringBuilder)
+     using stream api and method reference
+     abcd become dcba
+      */
+    public String reverseEachWords(String strLine) {
+        String[] strArray = strLine.split(" ");
+        ReverseString reverseString = new ReverseString();
         return Stream.of(strArray)
                 .map(reverseString::reverseWithoutInbuiltInUsingStreamApi)
-                .map(s->s.concat(" "))
+                .map(s -> s.concat(" "))
                 .collect(Collectors.joining());
+    }
+
+    /*
+     Reverse each word in a line  without using java built in method reverse(in StringBuilder)
+     using stream api in one method pipeline
+     abcd become dcba
+      */
+    public String reverseEachWordsInALine(String line) {
+        String[] strArray = line.split(" ");
+        return Stream.of(strArray)
+                .map(s -> IntStream.iterate(s.length() - 1, i -> i >= 0, i -> i = i - 1)
+                        .boxed()
+                        .map(s::charAt)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining()))
+                .map(s -> s.concat(" "))
+                .collect(Collectors.joining());
+
     }
 }
