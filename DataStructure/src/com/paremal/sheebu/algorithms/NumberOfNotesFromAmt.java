2764fe7893eb1a447c7593b6e3471ef1adbe5f38
Sheebu PJ <sheebupj@gmail.com>
2025-07-16 17:24:39 +0530

    get notes count from amount is rewritten with IntStreams methods

--- DataStructure/src/com/paremal/sheebu/algorithms/NumberOfNotesFromAmt.java
+++ DataStructure/src/com/paremal/sheebu/algorithms/NumberOfNotesFromAmt.java
@@ -2,18 +2,22 @@ package com.paremal.sheebu.algorithms;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class NumberOfNotesFromAmt {
     public static void main(String[] args) throws InterruptedException {
 
         Map<Integer,Integer> notesMap=getNumberOfNotesFrAmt(2888);
         notesMap.forEach((key, value) -> System.out.println(key + ": " + value));
+        Map<Integer,Integer> notesMap1=getNumberOfNotesFrAmtStream(2888);
+        notesMap1.forEach((key, value) -> System.out.println(key + ": " + value));
 
     }
 
     static Map<Integer,Integer> getNumberOfNotesFrAmt(Integer amt){
         Map<Integer,Integer> notesMap=getNotesMap();
-        Integer value=0, noteCount;
+        Integer value, noteCount;
         Map<Integer,Integer> result= new LinkedHashMap<>();
         for(Map.Entry<Integer,Integer> noteEntry:notesMap.entrySet()) {
             value = noteEntry.getValue();
@@ -23,6 +27,22 @@ public class NumberOfNotesFromAmt {
                 amt = amt - (noteCount * value);
             }
         }
+        return result;
+    }
+    static Map<Integer,Integer> getNumberOfNotesFrAmtStream(Integer amt){
+        Map<Integer,Integer> notesMap=getNotesMap();
+        AtomicInteger atmicAmt= new AtomicInteger(amt);
+        Map<Integer,Integer> result= new LinkedHashMap<>();
+        int len= notesMap.entrySet().size();
+        IntStream.iterate(0,i->i<len,i-> i=i+1)
+                .forEach(i->{
+                  Integer value=notesMap.get(i);
+                    if (atmicAmt.get() >= value) {
+                         int noteCount = atmicAmt.get() / value;
+                        result.put(value, noteCount);
+                        atmicAmt.set( atmicAmt.get() - (noteCount * value));
+                    }
+                });
         return result;
     }
     private static Map<Integer, Integer> getNotesMap() {
